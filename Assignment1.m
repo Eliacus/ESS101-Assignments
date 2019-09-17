9d3a979d748dc7d125c4b43df50cc5524f3cf4a9
crnlej <lejla.crnic@hotmail.com>
2019-09-17 09:22:05 +0200

    Update Matlab

--- Assignment1.m
+++ Assignment1.m
@@ -1,6 +1,8 @@
 %% Question 1a
 
-syms theta phi L phi_dot theta_dot m2 g m1
+syms theta phi L phi_dot theta_dot m2 g m1 u real
+
+u = sym('u',[3,1],'real');
 
 p_1 = sym('p_1',[3,1],'real');
 p_1_dot = sym('p_1_dot',[3,1],'real');
@@ -10,7 +12,7 @@ q_dot = [p_1_dot; theta_dot; phi_dot];
 
 p_2 = [p_1(1) + cos(theta)*sin(phi)*L;
       p_1(2) + sin(theta)*sin(phi)*L;
-       p_1(3)-L*cos(phi)]; %p_1(3) affects??
+       p_1(3)-L*cos(phi)]; 
 p_2_dot = jacobian(p_2,q)*q_dot;
 
 T_1 = (1/2)*m1*(p_1_dot)'*p_1_dot; 
@@ -22,5 +24,24 @@ V_2 = m2*g*[0 0 1]*p_2;
 L = (T_1+T_2)-(V_1+V_2);
 
 L_gradient_q_dot = (jacobian(L,q_dot))';
+W_jacobian = jacobian(L_gradient_q_dot,q);
 
+F = [u; 0; 0];
+
+L_gradient_q  = (jacobian(L,q))';
+L_gradient_q_dot_dt = F+L_gradient_q;
+
+Mv = L_gradient_q_dot_dt - W_jacobian*q_dot;
+
+simplify(Mv)
+
+
+%% Question 1b 
+
+q_new = [p_1; p_2];
+q_new_dot = [p_1_dot; p_2_dot];
+e = p_1 - p_2;
+
+C = (1/2)*(e'*e-L^2);
+C = simplify(C);
 
